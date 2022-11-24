042f97f9c912efd3476c7dfaa57d539a6b0d0210
Santhosh S <59478954+santhosh420@users.noreply.github.com>
2022-11-24 17:00:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+//Create your own resume data in JSON format.
+var text = {
+    "Name" : "Santhosh",
+    "Age" : "20",
+    "Gender" : "Male",
+    "Acadmeic" : "B.E Electronic and Communication Engineering",
+    "Email" : "santhosh@gmail.com",
+    "Area of Interest" : "Full stack Developer",
+    "Language" : "Tamil, English",
+    "Web technologies" : "HTML, CSS, Javascript",
+    "Social media" : "Instagram, Linkedin",
+    "Hobbies" : "Reading comics and Watching anime",
+    "Year of passed out" : "2022",
+    "Operating system known" : "Windows and kali linux",
+    "Programming language" : "Jvascript, Python",
+    "Address" : "XXX,YYY",
+    "Date of birth" : "06-02-2000"
+}
+console.log(text);
